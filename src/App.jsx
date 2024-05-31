87fb1d212e6920bf9bec9bc61e461b517e103e13
jpabon <jpabon>
2024-05-30 20:45:31 -0500

    Registro de usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,17 @@ import Conversor from './Conversor'
 function App() {
   const [usuario, setUsuario] = useState('')
   const [clave, setClave] = useState('')
+  const [usuarioRegistro, setUsuarioRegistro] = useState('')
+  const [claveRegistro, setClaveRegistro] = useState('')
   const [logueado, setLogueado] = useState(false)
+
+  function cambiarUsuarioRegistro(evento) {
+    setUsuarioRegistro(evento.target.value)
+  }
+
+  function cambiarClaveRegistro(evento) {
+    setClaveRegistro(evento.target.value)
+  }
 
   function cambiarUsuario(evento) {
     setUsuario(evento.target.value)
@@ -24,12 +34,16 @@ function App() {
     } else {
       alert('Usuario o clave incorrectos')
     }
-    // if (usuario == 'admin' && clave == 'admin') {
-    //   alert('Ingresaste')
-    //   setLogueado(true)
-    // } else {
-    //   alert('Usuario o clave incorrectos')
-    // }
+  }
+
+  async function registro() {
+    const peticion = await fetch('http://localhost:3000/registrar?usuario=' + usuarioRegistro + '&clave=' + claveRegistro, { credentials: 'include' })
+    if (peticion.ok) {
+      alert('Usuario registrado')
+      setLogueado(true)
+    } else {
+      alert('No se pudo registrar el usuario')
+    }
   }
 
   async function validar() {
@@ -44,7 +58,13 @@ function App() {
   }, [])
 
   if (logueado) {
-    return <Conversor />
+    return (<div>
+      <h1>Registro</h1>
+      <input placeholder='Usuario' type="text" name="usuario" id="usuario" value={usuarioRegistro} onChange={cambiarUsuarioRegistro} />
+      <input placeholder='Clave' type="password" name="clave" id="clave" value={claveRegistro} onChange={cambiarClaveRegistro} />
+      <button onClick={registro}>Registrar</button>
+      <Conversor />
+    </div>)
   }
 
   return (
@@ -53,6 +73,8 @@ function App() {
       <input placeholder='Usuario' type="text" name="usuario" id="usuario" value={usuario} onChange={cambiarUsuario} />
       <input placeholder='Clave' type="password" name="clave" id="clave" value={clave} onChange={cambiarClave} />
       <button onClick={ingresar}>Ingresar</button>
+
+
     </>
   )
 }
